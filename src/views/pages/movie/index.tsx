02d475ac2739f8d9b0aab4f1b45a462a63f3e76c
Ittipol Vongapai <itti.task@gmail.com>
2023-12-18 07:47:43 +0700

    add search

--- src/views/pages/movie/index.tsx
+++ src/views/pages/movie/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchMovies } from "../../../store/moviesActions";
 import { AppDispatch } from "../../../store";
@@ -10,6 +10,15 @@ import { MovieModel } from "../../../models";
 export default function Movie() {
   const dispatch: AppDispatch = useDispatch();
   const movies = mock.movies;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  useEffect(() => {
+    const filtered = movies.filter((movie) =>
+      movie.title_en.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredMovies(filtered);
+  }, [searchQuery, movies]);
 
   const formatMovie = (movie: MovieModel) => ({
     ...movie,
@@ -25,8 +34,15 @@ export default function Movie() {
     <div className="w-full flex flex-row flex-grow">
       <div className="w-full xl-max:w-2/3 px-[20px] sm:px-12 md:px-18 lg:px-32 pt-[72px] md:pt-14 flex flex-col justify-between flex-grow">
         <Stack spacing={2} className="flex-1 ml-[1px] mt-[1px]">
+          <input
+            type="text"
+            placeholder="Search movies..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="search-input" // Add appropriate styling class
+          />
           <div className="movies-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
-            {movies.map((movie) => (
+            {filteredMovies.map((movie) => (
               <MovieCard
                 key={movie.id}
                 movie={formatMovie(movie as MovieModel)}
