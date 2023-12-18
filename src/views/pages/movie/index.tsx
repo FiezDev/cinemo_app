14f172062561ddd053d7393b4885c7bfefaf661d
Ittipol Vongapai <itti.task@gmail.com>
2023-12-18 11:01:19 +0700

    updated movie index with api

--- src/views/pages/movie/index.tsx
+++ src/views/pages/movie/index.tsx
@@ -1,23 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { fetchMovies } from "../../../store/moviesActions";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../../store";
-import mock from "../../../models/mock.json";
+import { fetchMovies } from "../../../store/moviesActions";
 import MovieCard from "../../components/MovieCard";
-import { Divider, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { MovieModel } from "../../../models";
+import { RootState } from "../../../store";
 
 export default function Movie() {
   const dispatch: AppDispatch = useDispatch();
-  const movies = mock.movies;
+  const movies = useSelector((state: RootState) => state.movies.movies);
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredMovies, setFilteredMovies] = useState(movies);
+  const [filteredMovies, setFilteredMovies] = useState<MovieModel[]>([]);
 
   useEffect(() => {
-    const filtered = movies.filter((movie) =>
-      movie.title_en.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setFilteredMovies(filtered);
+    dispatch(fetchMovies());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (movies) {
+      const filtered = movies.filter((movie) =>
+        movie.title_en.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredMovies(filtered);
+    }
   }, [searchQuery, movies]);
 
   const formatMovie = (movie: MovieModel) => ({
@@ -26,33 +32,22 @@ export default function Movie() {
     trailer_ivx_key: movie.trailer_ivx_key || "nokey",
   });
 
-  useEffect(() => {
-    dispatch(fetchMovies());
-  }, [dispatch]);
-
   return (
     <div className="w-full flex flex-row flex-grow">
-      <div className="w-full xl-max:w-2/3 px-[20px] sm:px-12 md:px-18 lg:px-32 pt-[72px] md:pt-14 flex flex-col justify-between flex-grow">
-        <Stack spacing={2} className="flex-1 ml-[1px] mt-[1px]">
-          <input
-            type="text"
-            placeholder="Search movies..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="search-input" // Add appropriate styling class
-          />
-          <div className="movies-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
-            {filteredMovies.map((movie) => (
-              <MovieCard
-                key={movie.id}
-                movie={formatMovie(movie as MovieModel)}
-              />
-            ))}
-          </div>
-          <div className="block md:hidden flex-1"></div>
-          <Divider className="md:mt-[46px] mb-[77px]"></Divider>
-        </Stack>
-      </div>
+      <Stack spacing={2} className="flex-1 ml-[1px] mt-[1px]">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className=""
+        />
+        <div className="movies-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
+          {filteredMovies.map((movie) => (
+            <MovieCard key={movie.id} movie={formatMovie(movie)} />
+          ))}
+        </div>
+      </Stack>
     </div>
   );
 }
