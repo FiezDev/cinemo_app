70db6ed5d193e412ea759636fd8adc445f2f23c7
Ittipol Vongapai <itti.task@gmail.com>
2023-12-19 14:48:17 +0700

    updated slice

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import Layout from "../views/layout";
 import Movie from "../views/pages/movie";
 import Fav from "../views/pages/fav";
 import Login from "../views/pages/login";
+import Detail from "../views/pages/detail";
 
 export default function Root() {
   return (
@@ -14,6 +15,7 @@ export default function Root() {
           <Route path="movie" element={<Movie />} />
           <Route path="fav" element={<Fav />} />
           <Route path="login" element={<Login />} />
+          <Route path="detail/:id" element={<Detail />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/store/moviesSlice.ts
+++ src/store/moviesSlice.ts
@@ -3,10 +3,12 @@ import { MovieModel } from "../models";
 
 interface MoviesState {
   movies: MovieModel[];
+  favorites: number[];
 }
 
 const initialState: MoviesState = {
   movies: [],
+  favorites: JSON.parse(localStorage.getItem("favorites") || "[]"),
 };
 
 const movieSlice = createSlice({
@@ -16,7 +18,18 @@ const movieSlice = createSlice({
     setMovies(state, action: PayloadAction<MovieModel[]>) {
       state.movies = action.payload;
     },
+    toggleFavorite: (state, action: PayloadAction<number>) => {
+      const movieId = action.payload;
+      if (state.favorites.includes(movieId)) {
+        state.favorites = state.favorites.filter((id) => id !== movieId);
+      } else {
+        state.favorites.push(movieId);
+      }
+      localStorage.setItem("favorites", JSON.stringify(state.favorites));
+    },
   },
 });
+
+export const { setMovies, toggleFavorite } = movieSlice.actions;
 
 export default movieSlice;
