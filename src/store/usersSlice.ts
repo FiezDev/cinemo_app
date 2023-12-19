fe6306d7ca3bb32d64222302290ccf1efddf81a6
Ittipol Vongapai <itti.task@gmail.com>
2023-12-19 17:21:23 +0700

    updated state persist

--- src/store/usersSlice.ts
+++ src/store/usersSlice.ts
@@ -14,14 +14,34 @@ const usersSlice = createSlice({
   initialState,
   reducers: {
     login(state, action: PayloadAction<UserModel>) {
-      state.currentUser = action.payload;
+      const currentTime = new Date().getTime();
+      const userWithTimestamp = {
+        ...action.payload,
+        loginTime: currentTime,
+      };
+      localStorage.setItem("user", JSON.stringify(userWithTimestamp));
+      state.currentUser = userWithTimestamp;
     },
     logout(state) {
+      localStorage.removeItem("user");
       state.currentUser = null;
+    },
+    initializeState(state) {
+      const userString = localStorage.getItem("user");
+      if (userString) {
+        const userWithTimestamp = JSON.parse(userString);
+        const currentTime = new Date().getTime();
+        if (currentTime - userWithTimestamp.loginTime < 7200000) {
+          state.currentUser = userWithTimestamp;
+        } else {
+          localStorage.removeItem("user");
+          state.currentUser = null;
+        }
+      }
     },
   },
 });
 
-export const { login, logout } = usersSlice.actions;
+export const { login, logout, initializeState } = usersSlice.actions;
 
 export default usersSlice;
